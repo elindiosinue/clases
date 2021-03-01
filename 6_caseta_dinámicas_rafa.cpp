88fe4038656fcf2096359298b6122338647932e9
Rafola <sarcastsv@gmail.com>
2021-03-01 21:34:31 +0100

    cambios

--- 6_caseta_dinámicas_rafa.cpp
+++ 6_caseta_dinámicas_rafa.cpp
@@ -1,9 +1,9 @@
 /*
-En una caseta de feria, se dispone de un fichero con los datos de los socios. Son estos:
+En una Caseta de feria, se dispone de un fichero con los datos de los socios. Son estos:
 
 -                  Codigo de socio (del 1 en adelante).
 -                  Nombre del socio (de 30 caracteres).
--                  Antiguedad en la caseta (anios).
+-                  Antiguedad en la Caseta (anios).
 -                  Cuota a pagar (en euros).
 
 Este fichero esta ordenado por codigo de socio, de forma que el codigo de socio coincide con la posicion del registro dentro del fichero.
@@ -18,10 +18,10 @@ Se terminara el proceso cuando se introduzca el codigo de socio 0.
 
 */
 
+#include <iostream>
+#include <cstring>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+using namespace std;
 
 #define CAR 30
 #define SUBIDA_MENOR 1.04
@@ -29,14 +29,14 @@ Se terminara el proceso cuando se introduzca el codigo de socio 0.
 #define SUBIDA_MAYOR 1.02
 
 typedef struct {
-    int codigo;
+    int codigo{};
     char nombre[CAR];
-    int antig;
-    int cuota;
-} caseta;
+    int antig{};
+    double cuota{};
+} Caseta;
 
 typedef struct nodo {
-    caseta datosSocio;
+    Caseta datosSocio;
     nodo *sig;
 } nodoSocio;
 
@@ -46,14 +46,15 @@ void mostrar_menu() {
     printf("--------\n");
     printf("1). Alta socio.\n");
     printf("2). Actualizar cuota de socio.\n");
+    printf("3). Leer la lista.\n");
+    printf("0). salir.\n");
 }
 
 void leer_opcion(int *op) {
     do {
-        printf("\nIntroduzca una opcion del menu: ");
-        scanf("%d", op);
-        fflush(stdin);
-    } while ((*op) != 1 && (*op) != 2);
+        std::cout << "Introduzca una opcion del menu: ";
+        std::cin >> *op;
+    } while ((*op) < 0 || (*op) > 3);
 }
 
 void insertar_nodo_en_lista(nodoSocio **lista, nodoSocio *aux) {
@@ -74,40 +75,30 @@ void insertar_nodo_en_lista(nodoSocio **lista, nodoSocio *aux) {
         }
     }
     printf("\nNuevo socio dado de alta correctamente.\n");
-
-    // mostrar_lista (borrar)
-    /*nodoSocio *mostrar = *lista;
-    while (mostrar != NULL)
-    {
-        printf("%d - %s - %d - %d\n", mostrar->datosSocio.codigo, mostrar->datosSocio.nombre, mostrar->datosSocio.antig, mostrar->datosSocio.cuota);
-        mostrar = mostrar->sig;
-    }*/
 }
 
 void pedir_codigo(int *codigo) {
     do {
-        printf("\nIntroduzca el codigo (0 para salir): ");
-        scanf("%d", codigo);
-        fflush(stdin);
+        std::cout << "Introduzca el codigo (0 para salir): ";
+        std::cin >> *codigo;
     } while (*codigo < 0);    // por si mete el cero para salir, que no se quede en bucle.
 }
 
 void pedir_nombre(char *nombre) {
-    printf("\nIntroduzca el nombre: ");
-    gets(nombre);
-    fflush(stdin);
+    std::cout << "Introduzca el nombre: ";
+    std::cin >> nombre;
 }
 
 void pedir_antiguedad(int *antig) {
-    printf("\nIntroduzca la antiguedad: ");
-    scanf("%d", antig);
-    fflush(stdin);
+    std::cout << "Introduzca la antiguedad: ";
+    std::cin >> *antig;
+
 }
 
-void pedir_cuota(int *cuota) {
-    printf("\nIntroduzca la cuota: ");
-    scanf("%d", cuota);
-    fflush(stdin);
+void pedir_cuota(double *cuota) {
+    std::cout << "Introduzca la cuota: ";
+    std::cin >> *cuota;
+
 }
 
 void insertar_fichero_en_lista(nodoSocio **lista, nodoSocio *nuevo) {
@@ -154,11 +145,12 @@ void calcular_cuota(nodoSocio **encontrado) {
 }
 
 nodoSocio *buscar_nodo(nodoSocio *lista, int codigo) {
-    while (lista != NULL) {
-        if (lista->datosSocio.codigo == codigo)
-            return lista;
+    nodoSocio *aux = lista;
+    while (aux != NULL) {
+        if (aux->datosSocio.codigo == codigo)
+            return aux;
         else
-            lista = lista->sig;
+            aux = aux->sig;
     }
     return NULL;
 }
@@ -182,8 +174,16 @@ void actualizar_cuota_socio(nodoSocio *lista) {
         printf("\nEl codigo de socio no existe.\n");
     else {
         calcular_cuota(&encontrado);
-        printf("\nLa cuota del socio %d se ha actualizado: %d euros.\n", encontrado->datosSocio.codigo,
+        printf("\nLa cuota del socio %d se ha actualizado: %f euros.\n", encontrado->datosSocio.codigo,
                encontrado->datosSocio.cuota);
+    }
+}
+
+void leer_lista(nodoSocio *lista) {
+    nodoSocio *aux = lista;
+    while (aux != NULL) {
+        printf("\nSocio : %d de nombre: %s con una antiguedad de %d y una cuota de %f.\n", aux->datosSocio.codigo, aux->datosSocio.nombre, aux->datosSocio.antig, aux->datosSocio.cuota);
+        aux = aux->sig;
     }
 }
 
@@ -194,10 +194,14 @@ void tratar_opcion(nodoSocio **lista, int *op) {
             break;
         case 2:
             actualizar_cuota_socio(*lista);
+            break;
+        case 3:
+            leer_lista(*lista);
+            break;
     }
 }
 
-void crear_nodo(nodoSocio **lista, caseta aux) {
+void crear_nodo(nodoSocio **lista, Caseta aux) {
     nodoSocio *nuevo = (nodoSocio *) malloc(sizeof(nodoSocio));
 
     nuevo->datosSocio.codigo = aux.codigo;
@@ -216,35 +220,35 @@ void leer_fichero_existente(nodoSocio **lista) {
     if (f == NULL)
         return;        // No imprimimos nada porque haya datos o no, se va a abrir el archivo y al usuario no le interesa
 
-    caseta aux;
+    Caseta aux;
 
-    fread(&aux, sizeof(caseta), 1, f);
+    fread(&aux, sizeof(Caseta), 1, f);
 
     while (!feof(f)) {
-        //printf("%d - %s - %d - %d\n", aux.codigo, aux.nombre, aux.antig, aux.cuota);		// (control: para ver lo que habia en el fichero)
         crear_nodo(lista, aux);
-        fread(&aux, sizeof(caseta), 1, f);
+        fread(&aux, sizeof(Caseta), 1, f);
     }
     fclose(f);
 }
 
-int desea_seguir(int *s) {
+void desea_seguir(int *s) {
     do {
-        printf("\nDesea realizar mas tramites? (1 -> Si / 0 -> No): ");
-        scanf("%d", s);
-        fflush(stdin);
+        std::cout << "Desea realizar mas tramites? (1 -> Si / 0 -> No): ";
+        std::cin >> *s;
+
     } while ((*s != 0) && (*s != 1));
 }
 
 void volcar_lista_en_fichero(nodoSocio *lista) {
     FILE *f = fopen("feria.dat", "wb");
+    nodoSocio *aux = lista;
 
     if (f == NULL)
         printf("\nNo se ha encontrado el archivo. Se creara uno nuevo.\n");
 
-    while (lista != NULL) {
-        fwrite((&lista->datosSocio), sizeof(caseta), 1, f);
-        lista = lista->sig;
+    while (aux != NULL) {
+        fwrite((&aux->datosSocio), sizeof(Caseta), 1, f);
+        aux = aux->sig;
     }
     fclose(f);
 
